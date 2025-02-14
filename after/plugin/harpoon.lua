b6c9264deee878deba157b0c3669f0b492332a6a
Travia21 <travia21@gmail.com>
2025-02-13 23:49:01 -0500

    minor:Update harpoon quick jump shortcuts

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,11 +4,10 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
---Suggested by theprimeagen, but I don't like the maps
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-b>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-y>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-g>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-g>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(3) end)
+--vim.keymap.set("n", "<C-n>", function() ui.nav_file(4) end)
 
 -- settings
 require("harpoon").setup({
